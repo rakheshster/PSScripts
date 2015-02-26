fbee08af79953c628af2992807ccadbe5c7beabb
Rakhesh Sasidharan <me@rakhesh.com>
2015-02-26 10:46:42 +0400

    Changes to Download-File and Expand-Zip. Working so far, based on my minimal testing.

---
+++ Download-File.ps1
@@ -0,0 +1,25 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$true)]
+  [ValidatePattern("^(http|https|ftp)\://.*$")]
+  [string]$Url,
+
+  [Parameter(Mandatory=$false)]
+  [string]$FileName,
+
+  [Parameter(Mandatory=$false)]
+  [switch]$PassThru
+)
+
+# Note: 
+# 1) I don't check if $FileName already exists or whether the path is correct. Maybe add that later ...
+
+if ($PassThru) { 
+    Invoke-WebRequest $Url
+} else {
+    if ($FileName) { 
+        Invoke-WebRequest $Url -OutFile $FileName
+    } else {
+        Invoke-WebRequest $url -OutFile $(Split-Path -Leaf $Url)
+    } 
+}
---
+++ Expand-Zip.ps1
@@ -0,0 +1,39 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$false,
+    ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True,
+    HelpMessage='A stream of bytes in lieu of the Zip file; can be piped to this script')]
+  [byte[]]$Content,
+
+  [Parameter(Mandatory=$false,
+    HelpMessage='The name of the Zip file')]
+  [ValidatePattern("^.*\.zip$")]
+  [string]$FileName,
+
+  [Parameter(Mandatory=$false,
+    HelpMessage='Absolute path to a pre-existing folder you would like to extract to')]
+  [ValidateScript({($_ -match "^[a-z]\:.*") -and (Test-Path -PathType Container $_)})]
+  [string]$ExpandTo
+)
+
+if (!$Content -and !$FileName) { throw "You must specify a filename or a stream of bytes. Aborting!" }
+
+if ($Content) {
+    $TempFile = Set-Content -Encoding Byte -Value $Content -Path "${Env:\TEMP}\$(Get-Random).zip" -PassThru
+    $FileName = $TempFile.PSPath
+}
+
+$Shell = New-Object -ComObject shell.application
+$ZipFile = $Shell.NameSpace($FileName)
+
+ForEach ($Item in $ZipFile.items()) {
+    $Shell.Namespace($ExpandTo).copyhere($Item)
+}
+
+if ($Content) { Remove-Item -Force $TempFile.PSPath }
+
+## TODO:
+## <nothing as of now>
+
+## INSPIRED BY:
+## http://www.howtogeek.com/tips/how-to-extract-zip-files-using-powershell/
