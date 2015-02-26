64def7c8f426c6d840447d9ec6a64ce4ab0421d7
Rakhesh Sasidharan <me@rakhesh.com>
2015-02-26 09:01:16 +0400

    Added Expand-ZipFile and Update-Hosts

---
+++ Expand-ZipFile.ps1
@@ -0,0 +1,12 @@
+﻿# via http://www.howtogeek.com/tips/how-to-extract-zip-files-using-powershell/
+Function Expand-ZipFile($file, $destination) 
+{
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($file)
+    foreach($item in $zip.items()) {
+        $shell.Namespace($destination).copyhere($item)
+    }
+}
+
+## TODO:
+## 1) Take the inputs from pipeline so I can download something and unzip
